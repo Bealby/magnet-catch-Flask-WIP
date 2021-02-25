ddca1fc9537dee6c3ed6e7557a786a786440316b
Bealby <mgbealby@gmail.com>
2021-02-25 12:22:17 +0000

    Create register.html. Set-up app.py funcitonality

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+# Imports
+
 import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+# 'MONGO_URI' and 'EMAILJS_KEY' stored in Enviroment Variables
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -16,12 +22,21 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# Function to load 'Home' page as default
+
 
 @app.route('/')
 def home():
     countries = mongo.db.countries.find()
-    return render_template("intro-communication/index.html",
+    return render_template("index.html",
                            countries=countries)
+
+# Function to load 'Register' page
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
