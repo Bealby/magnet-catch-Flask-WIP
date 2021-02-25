1c3ce0b19b32046c544e40e72772cbbbf21253b5
Bealby <mgbealby@gmail.com>
2021-02-25 20:11:47 +0000

    Add if session.user for nav display

--- app.py
+++ app.py
@@ -86,6 +86,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
@@ -96,6 +104,8 @@ def profile(username):
         return render_template("profile.html", username=username)
 
     return redirect(url_for("login"))
+
+
 
 
 if __name__ == "__main__":
